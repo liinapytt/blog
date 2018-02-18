ff8dc2500c59ac32de4e724c78e650a067ec9bf1
Liina Püttsepp <liinareb@gmail.com>
2018-02-18 13:39:23 +0200

    Add view/tags_index file to tags directory

--- controllers/tags.php
+++ controllers/tags.php
@@ -11,12 +11,12 @@ namespace App;
 
 class tags extends Controller
 {
-    function index(){
+    function index()
+    {
         $this->tags = get_all(
             "SELECT tag_name, ".
-            "COUNT(post_id) AS count".
-            "FROM post_tags NATURAL JOIN tag".
+            "COUNT(post_id) AS count ".
+            "FROM post_tags NATURAL JOIN tag ".
             "GROUP BY tag_id");
-        )
-    }
+        }
 }
