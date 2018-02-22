81b92a0a00ae3221cc2a55c6e09dc6e9c9477c99
Liina Püttsepp <liinareb@gmail.com>
2018-02-22 14:41:31 +0200

    Add tags view

--- views/tags/tags_index.php
+++ views/tags/tags_index.php
@@ -1,5 +1,6 @@
 <ul class="list-group">
+
     <?php foreach($tags as $tag): ?>
-    <a href="#"><li class="list-group-item active"><?php echo $tag['tag_name']; ?></li></a>
+    <a href="<?=BASE_URL?>tags/view/<?=$tag['tag_id']?>"><li class="list-group-item"><?php echo $tag['tag_name']; ?></li></a>
     <?php endforeach; ?>
 </ul>
